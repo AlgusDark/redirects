ff4c1bbec5479b8a7b4811d901a7406959d6a000
Carlos Perez Gutierrez <carlos@medmain.com>
2020-11-07 18:38:07 +0900

    Add Inter typography

--- stylelint.config.js
+++ stylelint.config.js
@@ -17,7 +17,7 @@ module.exports = {
     "font-family-no-missing-generic-family-keyword": [
       true,
       {
-        ignoreFontFamilies: ["Alegreya", "system"],
+        ignoreFontFamilies: ["Alegreya", "Inter"],
       },
     ],
     "declaration-block-trailing-semicolon": null,
